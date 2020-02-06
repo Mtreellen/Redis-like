6724589631e12d965ee0f29728c916c21e8975d0
Mtreellen <mtreellen@163.com>
2020-02-06 21:02:50 +0800

    仿redis项目

---
+++ MiniRedis/src/main/java/com/ellen/miniredis/commands/LPUSHCommand.java
@@ -0,0 +1,31 @@
+package com.ellen.miniredis.commands;
+
+import com.ellen.miniredis.Command;
+import com.ellen.miniredis.Database;
+import com.ellen.miniredis.Protocol;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+public class LPUSHCommand implements Command {
+    private List<Object> args;
+    @Override
+    public void setArgs(List<Object> args) {
+        this.args = args;
+    }
+
+    @Override
+    public void run(OutputStream os) throws IOException {
+        if (args.size() != 2){
+            Protocol.writeError(os,"the size should be two");
+            return;
+        }
+        String key = new String((byte[])args.get(0));
+        String value = new String((byte[])args.get(1));
+        List<String> list = Database.getList(key);
+        //头插法将value数据插入list中
+        list.add(0,value);
+        Protocol.writeInteger(os,list.size());
+    }
+}
